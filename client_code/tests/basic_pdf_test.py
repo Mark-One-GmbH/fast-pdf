b1151ec7812b2a12e89b6c01fe24fb4b4e723fff
mark.breuss@markone.at <mark.breuss@markone.at>
2023-03-08 13:47:59 +0000

    Edited fast_pdf.jspdf, s_tests and tests.basic_pdf_test

--- client_code/tests/basic_pdf_test.py
+++ client_code/tests/basic_pdf_test.py
@@ -3,10 +3,15 @@ from ..fast_pdf import Document
 from datetime import datetime
 
 
-def basic_document(company_logo):
+def basic_document():
   '''
   Returns a Document Instance that contains basic layouts
   '''
+
+  #get Header logo
+  import anvil.media
+  company_logo = anvil.URLMedia('https://static.wixstatic.com/media/7a2c3b_422826ce39644fc080d206c80ce59612~mv2.png/v1/fill/w_462,h_455,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/Kopie%20von%20M1_standard_black_transparent%20background_low.png')
+  
   #0. Define some common variables
   line_h = 5 #standard line height
   
@@ -26,7 +31,7 @@ def basic_document(company_logo):
   doc.set_font('Arial',9)
   
   #3. Main Document Body
-  for i in range(5):
+  for i in range(50):
     doc.add_page()
     #Adress Block
     doc.spacer(20)
@@ -66,12 +71,8 @@ def basic_document(company_logo):
 #################
 def run_test():
   '''returns an anvil blob media pdf'''
- #get Header logo
-  import anvil.media
-  company_logo = anvil.URLMedia('https://static.wixstatic.com/media/7a2c3b_422826ce39644fc080d206c80ce59612~mv2.png/v1/fill/w_462,h_455,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/Kopie%20von%20M1_standard_black_transparent%20background_low.png')
-  
   client_start = datetime.now()
-  doc = basic_document(company_logo)
+  doc = basic_document()
   client_blob = doc.to_blob()
   print('client done',datetime.now()-client_start)
 
