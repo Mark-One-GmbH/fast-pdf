fc4767c4682eb7bf5c41f2f29f4d02c9ded46add
mark.breuss@markone.at <mark.breuss@markone.at>
2023-03-11 18:35:49 +0000

    Edited fast_pdf.fpdf

--- client_code/fast_pdf/fpdf.py
+++ client_code/fast_pdf/fpdf.py
@@ -6,22 +6,21 @@ class CustomFPDF(FPDF):
       if not hasattr(self, 'footer_callback') or self.header_callback is None: return
       self.footer_callback(self)
     except Exception as e:
-      print('footer error',e,self.page_no())
-      pass
-
+      print('footer error',e)
+      
   def header(self):
     try:
       if not hasattr(self, 'header_callback') or self.header_callback is None: return
       self.header_callback(self)
     except Exception as e:
-      print('header error',e,self.page_no())
-
+      print('header error',e)
+      
   def add_image(self,image_data,x=0,y=0,w=0,h=0,alias='',compression='MEDIUM',rotation=0):
     '''Takes an image in form of a blob and prints it on the pdf'''
     from . import utils
     pil_img = utils.media_obj_to_pil(image_data)
     self.image(pil_img,x,y,w,h)
-
+    
   def vertical_text(self,width,height,text,border=0,ln=0,align='L',fill=False):
     self.rotate(90)
     self.cell(width, height, text, border =border, ln=ln, align=align, fill=fill)
